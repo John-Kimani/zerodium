aca01dec883af390f2c327ccc91df6192f7d8540
John-Kimani <kimwaweru100@gmail.com>
2022-12-10 14:23:05 +0300

    App: Development implements routing, 404 error handling and application base styling

--- frontend/zerodium/src/App.js
+++ frontend/zerodium/src/App.js
@@ -1,7 +1,27 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+
+import { BrowserRouter, Route, Routes} from "react-router-dom";
+import NavBar from './components/navbar/Navbar';
+import HomePage from './pages/HomePage/Homepage';
+import PageNotFound from './components/error/PageNotFound';
+import ComingSoon from './components/comingsoon/ComingSoon';
+
 function App() {
   return (
     <div className="App">
-      <h1>Home Page</h1>
+      <BrowserRouter>
+        {/* Toast container */}
+        {/* Navigation */}
+        <NavBar />
+        <Routes>
+          <Route exact path='/coming-soon' element={ <ComingSoon />}/>
+            {/* Blogs */}
+            <Route exact path='/' element={ <HomePage />}/>
+            {/* Page not found */}
+            <Route path='*' element={ <PageNotFound />}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
