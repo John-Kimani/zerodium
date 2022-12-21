0ba66c2c19aee61f6ae2720d87f7ae0543d68eb6
John-Kimani <kimwaweru100@gmail.com>
2022-12-21 16:10:57 +0300

    App: Fix: svg classname attributues

--- frontend/zerodium/src/components/services/Services.jsx
+++ frontend/zerodium/src/components/services/Services.jsx
@@ -95,7 +95,7 @@ const Services = () => {
                   width="45"
                   height="45"
                   fill="currentColor"
-                  class="bi bi-book-half"
+                  className="bi bi-book-half"
                   viewBox="0 0 16 16"
                 >
                   <path d="M8.5 2.687c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492V2.687zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z" />
@@ -121,7 +121,7 @@ const Services = () => {
                   width="45"
                   height="45"
                   fill="currentColor"
-                  class="bi bi-flag-fill"
+                  className="bi bi-flag-fill"
                   viewBox="0 0 16 16"
                 >
                   <path d="M14.778.085A.5.5 0 0 1 15 .5V8a.5.5 0 0 1-.314.464L14.5 8l.186.464-.003.001-.006.003-.023.009a12.435 12.435 0 0 1-.397.15c-.264.095-.631.223-1.047.35-.816.252-1.879.523-2.71.523-.847 0-1.548-.28-2.158-.525l-.028-.01C7.68 8.71 7.14 8.5 6.5 8.5c-.7 0-1.638.23-2.437.477A19.626 19.626 0 0 0 3 9.342V15.5a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 1 0v.282c.226-.079.496-.17.79-.26C4.606.272 5.67 0 6.5 0c.84 0 1.524.277 2.121.519l.043.018C9.286.788 9.828 1 10.5 1c.7 0 1.638-.23 2.437-.477a19.587 19.587 0 0 0 1.349-.476l.019-.007.004-.002h.001" />
@@ -148,7 +148,7 @@ const Services = () => {
                   width="45"
                   height="45"
                   fill="currentColor"
-                  class="bi bi-funnel"
+                  className="bi bi-funnel"
                   viewBox="0 0 16 16"
                 >
                   <path d="M1.5 1.5A.5.5 0 0 1 2 1h12a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.128.334L10 8.692V13.5a.5.5 0 0 1-.342.474l-3 1A.5.5 0 0 1 6 14.5V8.692L1.628 3.834A.5.5 0 0 1 1.5 3.5v-2zm1 .5v1.308l4.372 4.858A.5.5 0 0 1 7 8.5v5.306l2-.666V8.5a.5.5 0 0 1 .128-.334L13.5 3.308V2h-11z" />
@@ -166,18 +166,6 @@ const Services = () => {
               </Card.Body>
             </Card>
           </div>
-          {/* {ServicesData.map(({ id, serviceName, serviceDesc }) => {
-            return (
-              <div className="col-sm-12 col-md-4 mt-4">
-                <Card key={id} className='service-card'>
-                  <Card.Body>
-                    <Card.Title className="service_card-name">{serviceName}</Card.Title>
-                    <Card.Text className="service_card-desc">{serviceDesc}</Card.Text>
-                  </Card.Body>
-                </Card>
-              </div>
-            );
-          })} */}
         </div>
       </div>
     </main>
