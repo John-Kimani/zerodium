6ffdac3ae8156b561111bbda9373ea3a285221e7
John-Kimani <kimwaweru100@gmail.com>
2022-12-21 14:55:46 +0300

    App: revamp footer

--- frontend/zerodium/src/components/footer/Footer.jsx
+++ frontend/zerodium/src/components/footer/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <footer>
       <div className="container container-fluid">
-        <div className="row">
+        <div className="row" id="footer__content">
           <div className="col-sm-12 col-md-8">
             <form action="" className="get_in_touch">
               <div className="row">
@@ -34,32 +34,62 @@ const Footer = () => {
                 ></textarea>
               </div>
 
-              <div className="mt-4 d-grid gap-2">
-                <Button
-                  variant="outline-secondary"
-                  size="lg"
-                  className="contact_us-btn"
-                >
+              <div className="mt-4">
+                <Button variant="outline-secondary" className="contact_us-btn">
                   Send
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="22"
+                    height="22"
+                    fill="currentColor"
+                    className="bi bi-send"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z" />
+                  </svg>
                 </Button>
               </div>
             </form>
           </div>
           <div className="col-sm-12 col-md-4">
             <div className="contact__us">
-              <h5 className="contact-title">Call Us</h5>
-              <p className="contact-info"><i className="country-abv">K.E :</i> +(254) 795 344 966</p>
+              <h5 className="contact-title">
+                Call Us{" "}
+              </h5>
+              <p className="contact-info">
+              <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="25"
+                  height="25"
+                  fill="currentColor"
+                  class="bi bi-phone"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M11 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h6zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H5z" />
+                  <path d="M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
+                </svg><i className="country-abv">K.E :</i> +(254) 795 344 966
+              </p>
               <h5 className="contact-title">Follow our Social Media</h5>
               <div className="social_media">
-              <p className="contact-info">Twitter</p>
-              <p className="contact-info">Dribble</p>
-              <p className="contact-info">YouTube</p>
+                <p className="contact-info">Twitter</p>
+                <p className="contact-info">Dribble</p>
+                <p className="contact-info">YouTube</p>
               </div>
             </div>
           </div>
         </div>
+
         <div className="author-info">
-        <span className='footer__copy'>&#169; Site developed by <a href="https://twitter.com/kimperria" className='footer__copy-link'>Kimperria</a> || 2022</span>
+          <span className="footer__copy">
+            &#169; Site developed by{" "}
+            <a
+              href="https://twitter.com/kimperria"
+              className="footer__copy-link"
+            >
+              Kimperria
+            </a>{" "}
+            || 2022
+          </span>
         </div>
       </div>
     </footer>
