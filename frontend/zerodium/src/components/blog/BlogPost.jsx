94afd44b92df3fb343df0060decbb4d98a92ce30
John-Kimani <kimwaweru100@gmail.com>
2022-12-23 12:35:48 +0300

    App: Update cards endpoints

--- frontend/zerodium/src/components/blog/BlogPost.jsx
+++ frontend/zerodium/src/components/blog/BlogPost.jsx
@@ -1,33 +1,75 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
 import "./blogs.css";
-import { BlogPosts } from "./Data";
 
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
+import { Spinner } from "react-bootstrap";
 
 const BlogPost = () => {
+  const cardsBaseUrl = "https://zerodroidiumapi.up.railway.app/blogs/cards";
+
+  const [cards, setCards] = useState();
+
+  useEffect(() => {
+    axios.get(cardsBaseUrl).then((response) => {
+      if (response.status === 200) {
+        setCards(response.data);
+        // console.log(response)
+      } else {
+        setCards(null);
+      }
+    });
+  }, []);
+
   return (
     <main className="container container-fluid mt-2">
       <div className="row test">
-        {BlogPosts.map(({ id, blogImage, blogTitle,blogContent }) => {
-          return (
-            <div className="col-md-4 mt-4" key={id}>
-              <Card  className="blog_post">
-                <Card.Img variant="top" src={blogImage} className="blog_post-img img-fluid img-thumbnail"/>
-                <Card.Body>
-                  <Card.Title className="blog_title">{blogTitle}</Card.Title>
-                  <Card.Text className="blog_content">
-                    {blogContent }
-                  </Card.Text>
-                </Card.Body>
-                <Card.Footer>
-                <Button className="read_more-btn" variant="outline-secondary" size="md">
-                Read More
-              </Button>
-                </Card.Footer>
-              </Card>
-            </div>
-          )
-        })}
+        <>
+          {cards === undefined ? (
+            <Spinner animation="border" variant="light" />
+          ) : (
+            <>
+              {cards === null ? (
+                <p>Could not retrive articles.</p>
+              ) : (
+                <>
+                  {cards.map(({ id, blogImage, blogTitle, blogContent }) => {
+                    return (
+                      <div className="col-md-4 mt-4" key={id}>
+                        <Card className="blog_post">
+                          <Card.Img
+                            variant="top"
+                            src={blogImage}
+                            className="blog_post-img img-fluid img-thumbnail"
+                          />
+                          <Card.Body>
+                            <Card.Title className="blog_title">
+                              {blogTitle}
+                            </Card.Title>
+                            <Card.Text className="blog_content">
+                              {blogContent}
+                            </Card.Text>
+                          </Card.Body>
+                          <Card.Footer>
+                            <Button
+                              className="read_more-btn"
+                              variant="outline-secondary"
+                              size="md"
+                            >
+                              Read More
+                            </Button>
+                          </Card.Footer>
+                        </Card>
+                      </div>
+                    );
+                  })}
+                </>
+              )}
+            </>
+          )}
+        </>
       </div>
     </main>
   );
